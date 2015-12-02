9248238e01d6a1c16aa7baf6d68995a790314c6b
animatedantmo <animatedantmo@hotmail.com>
2015-12-01 22:25:46 -0500

    Added basic skeleton to Room class

--- src/objects/Room.java
+++ src/objects/Room.java
@@ -1,12 +1,57 @@
 package objects;
-import java.util.Set;
 
+import java.util.List;
 
 public class Room 
 {
-	Set<Player> playersInRoom;
-	
+	private String name;
+	private int id;
+	private Room secretConnectedRoom;
+	private List<Hallway> connectedHallways;
+	private List<Character> occupants;
+	//private List<Weapon> weapons;
+		
 	public Room()
 	{
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String newName) {
+		name = newName;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int newId) {
+		id = newId;
+	}
+	
+	public void addOccupant() {
+		
+	}
+	
+	public void removeOccupant() {
+		
+	}
+	
+	public void addWeapon() {
+		
+	}
+	
+	public void removeWeapon() {
+		
+	}
+	
+	public void getConnectedLocations() {
+		
+	}
+	
+	public void getOccupants() {
+		
 	}
 }
