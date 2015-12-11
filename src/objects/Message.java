ec76ebaf19dc8b0a2daf4e608a83f499e897027f
Ky To <kto5@jhu.edu>
2015-12-10 21:27:37 -0500

    delete actionID since we already have
    Action Enum field in Message class

--- src/objects/Message.java
+++ src/objects/Message.java
@@ -5,7 +5,6 @@ import java.util.*;
 @SuppressWarnings("serial")
 public class Message implements java.io.Serializable {
 	public int playerId;
-	public int actionId;
 	public List<Integer> guessInfo;
 	public List<Location> availableMoves;
 	public List<Location> playerLocations;
