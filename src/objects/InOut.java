7778fa6ac21234360d4f9aefe5c30c43db431b0b
animatedantmo <animatedantmo@hotmail.com>
2015-12-14 01:06:45 -0500

    Fixed some socket stuff

--- src/objects/InOut.java
+++ src/objects/InOut.java
@@ -2,14 +2,17 @@ package objects;
 
 import java.io.BufferedReader;
 import java.io.PrintWriter;
+import java.net.Socket;
 
 public class InOut {
 	public PrintWriter out;
 	public BufferedReader in;
 	public Player player;
+	public Socket socket;
 	
-	public InOut(BufferedReader in, PrintWriter out) {
+	public InOut(BufferedReader in, PrintWriter out, Socket socket) {
 		this.in = in;
 		this.out = out;
+		this.socket = socket;
 	}
 }
